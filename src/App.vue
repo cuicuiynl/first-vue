872be11902531516adff972872cacfb712141216
cuicuynl <cuicuiynl@163.com>
2019-01-25 15:45:06 +0800

    提交测试

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="哇咔哇咔呼啦呼啦零零落落"/>
+    <HelloWorld msg="哇咔哇咔呼啦呼啦零零落落略略略"/>
   </div>
 </template>
 
